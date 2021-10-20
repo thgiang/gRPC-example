4d7c11d016e5c2a737ef054360a3782fd5786f89
Tran Hoang Giang <tranhoanggiang@cryptopie-labo.com>
2021-10-21 00:25:35 +0700

    Huhu GRPC

---
+++ src/hero/hero.controller.ts
@@ -0,0 +1,81 @@
+import { Controller, Get, Inject, OnModuleInit, Param } from '@nestjs/common';
+import {
+  ClientGrpc,
+  GrpcMethod,
+  GrpcStreamMethod,
+} from '@nestjs/microservices';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { toArray } from 'rxjs/operators';
+import { HeroById } from './interfaces/hero-by-id.interface';
+import { Hero } from './interfaces/hero.interface';
+
+interface HeroService {
+  findOne(data: HeroById): Observable<Hero>;
+  findMany(upstream: Observable<HeroById>): Observable<Hero>;
+}
+
+@Controller('hero')
+export class HeroController implements OnModuleInit {
+  private readonly items: Hero[] = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Doe' },
+  ];
+  private heroService: HeroService;
+
+  constructor(@Inject('HERO_PACKAGE') private readonly client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.heroService = this.client.getService<HeroService>('HeroService');
+  }
+
+  @Get()
+  getMany(): Observable<Hero[]> {
+    const ids$ = new ReplaySubject<HeroById>();
+    ids$.next({ id: 1 });
+    ids$.next({ id: 2 });
+    ids$.complete();
+
+    const stream = this.heroService.findMany(ids$.asObservable());
+    return stream.pipe(toArray());
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string): Observable<Hero> {
+    return this.heroService.findOne({ id: +id });
+  }
+
+  @GrpcMethod('HeroService')
+  async findOne(data: HeroById): Promise<Hero> {
+    return await this.userServiceGetUserById(data.id)
+  }
+
+  @GrpcStreamMethod('HeroService')
+  findMany(data$: Observable<HeroById>): Observable<Hero> {
+    const hero$ = new Subject<Hero>();
+
+    const onNext = async (heroById: HeroById) => {
+
+      // TODO: Nếu dòng này dùng await (bỏ comment dòng dưới) sẽ ko ra data
+      const item = this.items.find(({id}) => id === heroById.id)
+      //const item = await this.userServiceGetUserById(heroById.id)
+
+      hero$.next(item);
+    };
+    const onComplete = () => hero$.complete();
+    data$.subscribe({
+      next: onNext,
+      complete: onComplete,
+    });
+
+    return hero$.asObservable();
+  }
+
+  userServiceGetUserById(heroId: number): Promise<Hero> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(this.items.find(({id}) => id === heroId))
+      }, 3000);
+    });
+  }
+}
+
